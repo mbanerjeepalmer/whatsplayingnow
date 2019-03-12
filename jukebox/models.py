0dbaa008cf2a84d55753c4a94269576e425b4057
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2019-03-12 00:48:26 +0000

    Try to get Heroku to apply the migration

--- jukebox/models.py
+++ jukebox/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+	# TODO maybe the blanks should be nulls...
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	spotify_id = models.CharField(max_length=255, blank=True, unique=True)
+	spotify_id = models.CharField(max_length=255, blank=True)
 	spotify_name = models.CharField(max_length=255, blank=True)
 	access_token = models.CharField(max_length=255, blank=True)
 	token_type = models.CharField(max_length=255, blank=True)
