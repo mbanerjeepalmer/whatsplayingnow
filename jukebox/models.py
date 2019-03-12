5001597a7afe9f6447d7c243d824bcf83feecb65
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2019-03-12 01:14:45 +0000

    Try unique spotify_id

--- jukebox/models.py
+++ jukebox/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 	# TODO maybe the blanks should be nulls...
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	spotify_id = models.CharField(max_length=255, blank=True)
+	spotify_id = models.CharField(max_length=255, blank=True, unique=True)
 	spotify_name = models.CharField(max_length=255, blank=True)
 	access_token = models.CharField(max_length=255, blank=True)
 	token_type = models.CharField(max_length=255, blank=True)
